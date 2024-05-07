fc9d8d65c5eb027b3206d1c0a23bfbc4d3eef618
Rishad Aufa Harahap <rishadaufaharahap@gmail.com>
2024-05-07 22:27:16 +0700

    Finished C10

---
+++ Challenge/c10.js
@@ -0,0 +1,30 @@
+function sentenceManipulation(sentence) {
+  const vowels = "aiueoAIUEO";
+  const sen = sentence.split(" ").map((word) => {
+    if (vowels.includes(word[0])) {
+      return word;
+    } else {
+      const str = word.split("");
+      const firstEl = str.shift();
+      str.push(firstEl);
+      return str.join("") + "nyo";
+    }
+  });
+  return sen.join(" ");
+}
+
+const readline = require("node:readline"),
+rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'tulis kalimatmu disini > '
+});
+rl.prompt()
+
+rl.on('line', (line) => {
+    console.log(`${sentenceManipulation(line)}`)
+    rl.prompt() 
+}).on('close', () => {
+    console.log("Good bye!")
+    process.exit(0)
+})
